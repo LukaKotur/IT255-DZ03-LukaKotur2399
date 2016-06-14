3781a7d99dd45c370ab112a5a18cc73738a27628
LukaKotur <looko95@gmail.com>
2016-06-14 18:43:47 +0200

    IT255-DZ03-LukaKotur2399

---
+++ build/classes/it255/dz03/LukaKotur22399/Main.rs
@@ -0,0 +1,1 @@
+IT255.DZ03.LukaKotur22399.Main
---
+++ src/it255/dz03/LukaKotur22399/RootObject.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IT255.DZ03.LukaKotur22399;
+
+/**
+ *
+ * @author Luka-PC
+ */
+public class RootObject {
+
+    private String origin;
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+}
